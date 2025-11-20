bf1b9e801a3c3eb1c49066b10bf7acfd42291fba
Lydia <lydiakirubai@gmail.com>
2025-11-20 10:58:02 +0530

    Variables

---
+++ src/learningday1/VariablesExample.java
@@ -0,0 +1,14 @@
+package learningday1;
+
+public class VariablesExample {
+	public static void main(String[] args) {
+    String name = "Lydia";
+    int age = 20;
+    char initial = 'S';
+    final float cgpa = 75.5f; //By using final keyword, the value of this variable cannot be changed
+    System.out.println("Hello " + name + " " + initial);
+    System.out.println("I am "+ age +" years old.");
+    System.out.println("My CGPA is "+cgpa);
+    
+	}
+}
