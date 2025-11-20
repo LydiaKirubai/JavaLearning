0b9931252b930c47b05796f0e18fc9bb02d5a567
Lydia <lydiakirubai@gmail.com>
2025-11-20 11:04:13 +0530

    final keyword

--- src/learningday1/VariablesExample.java
+++ src/learningday1/VariablesExample.java
@@ -5,7 +5,7 @@ public class VariablesExample {
     String name = "Lydia";
     int age = 20;
     char initial = 'S';
-    final float cgpa = 75.5f; //By using final keyword, the value of this variable cannot be changed
+    final float CGPA = 75.5f; //By using final keyword, the value of this variable cannot be changed
     System.out.println("Hello " + name + " " + initial);
     System.out.println("I am "+ age +" years old.");
     System.out.println("My CGPA is "+cgpa);
