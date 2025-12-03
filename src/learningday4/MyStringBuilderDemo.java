520f1ec21e3292d1d4e1caf7be29aa039482d823
Lydia <lydiakirubai@gmail.com>
2025-12-03 11:36:41 +0530

    learning

---
+++ src/learningday4/MyStringBuilderDemo.java
@@ -0,0 +1,13 @@
+package learningday4;
+
+public class MyStringBuilderDemo {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder("Hello");
+		sb.append(" World");
+		System.out.println(sb);
+		
+
+	}
+
+}
---
+++ src/learningday4/twoDAarray.java
@@ -0,0 +1,15 @@
+package learningday4;
+
+import java.util.Arrays;
+
+public class twoDAarray {
+
+	public static void main(String[] args) {
+		int[][] arr = new int[3][2];
+		arr[0][0] = 1;
+		arr[2][1] = 3;
+		
+		System.out.println(Arrays.deepToString(arr));
+	}
+
+}
